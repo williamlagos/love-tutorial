87b22b586f97ebcc3643a337b11b07cd8ded6c50
William Oliveira de Lagos <william.lagos@icloud.com>
2023-10-26 11:57:45 +0200

    feat: added controls to the game

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
+function love.load()
+  x = 100
+end
+
 function love.draw()
-  love.graphics.print("Hello World!", 300, 300)
+  love.graphics.print("Hello World!", x, 300)
+end
+
+function love.update()
+  -- if x < 400 then
+  --   x = x + 5
+  -- end
+  if love.keyboard.isDown("left") then
+    x = x - 5
+  end
+   if love.keyboard.isDown("right") then
+    x = x + 5
+  end
 end
